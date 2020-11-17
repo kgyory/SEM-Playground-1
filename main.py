383050cada416921c4d63c0cb4827c86ed8f5efc
Krisztian Gyoery <krisztian.gyoery@edu.campus02.at>
2020-11-17 17:00:22 +0100

    Nochmal print

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # ctrl + space: auto complete
 if __name__ == '__main__':
     print("hallo DAT")
+    print ("hallo Nochmal")
+
 # strg + shift + enter
 # alt + 3 taste: short help
 # shift shift: suchbox
