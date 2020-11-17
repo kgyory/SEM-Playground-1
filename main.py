e5d3c25574d3d1e2cf80037ade9c3e2e03c62f96
Krisztian Gyoery <krisztian.gyoery@edu.campus02.at>
2020-11-17 16:57:15 +0100

    Added my main file

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# TODO something
+# ctrl + space: auto complete
+if __name__ == '__main__':
+    print("hallo DAT")
+# strg + shift + enter
+# alt + 3 taste: short help
+# shift shift: suchbox
+# strg + shift + v: clipboard history
